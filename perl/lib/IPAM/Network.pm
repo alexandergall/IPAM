dac2daef7a6d0137b93c4092022255e8523a961a
Alexander Gall <alexander.gall@switch.ch>
2012-09-04 13:19:10 +0000

    Pimped perldoc.

--- perl/lib/IPAM/Network.pm
+++ perl/lib/IPAM/Network.pm
@@ -3,7 +3,7 @@
 #### Description:   IPAM::Network class
 #### Author:        Alexander Gall <gall@switch.ch>
 #### Created:       Jun 5 2012
-#### RCS $Id: Network.pm,v 1.4 2012/08/20 15:17:49 gall Exp gall $
+#### RCS $Id: Network.pm,v 1.5 2012/08/31 09:52:56 gall Exp gall $
 
 package IPAM::Network;
 use IPAM::Thing;
@@ -13,36 +13,36 @@ our @ISA = qw(IPAM::Thing);
 
 =head1 NAME
 
-IPAM::Network - Class that describes an IP stub network
+IPAM::Network - Class that describes an IP subnet
 
 =head1 SYNOPSIS
 
-use IPAM::Network;
+  use IPAM::Network;
 
 =head1 DESCRIPTION
 
 The L<IPAM::Network> class is derived from L<IPAM::Thing>.  It stores
-all information associated with an IP stub network, i.e. a network
-that contains hosts.  A network is associated with at most one IPv4
-and any number of IPv6 prefixes but at least one of either type (this
-is not enforced by the network object but should be enforced by the
-IPAM parser).  These prefixes are stored in a
-L<IPAM::Prefix::Registry>.  It also contains a L<IPAM::Host::Registry>
-that collects all hosts that have been defined in the context of the
-network.  It may also be associated with a geographic location that
-will be published as DNS LOC records for the hosts.
+all information associated with an IP subnet, i.e. a network that
+contains hosts.  A network is associated with at most one IPv4 and any
+number of IPv6 stub networks but at least one of either type (this is
+not enforced by the network object but should be enforced by the IPAM
+parser).  These prefixes are stored in a L<IPAM::Prefix::Registry>.
+It also contains a L<IPAM::Host::Registry> that collects all hosts
+that have been defined in the context of the network.  It may also be
+associated with a geographic location that will be published as DNS
+LOC records for the hosts.
 
 =head1 EXTENDED CLASS METHODS
 
 =over 4
 
-=item new($node, $name, $location)
+=item C<new($node, $name, $location)>
 
-my $net = IPAM::Network->new($node, $name, $loc);
+  my $net = IPAM::Network->new($node, $name, $loc);
 
-Creates a new instance and ssociates a geographic location in the form
-of a DNS LOC record with it.  Note that the location is not checked to
-be in valid DNS master file syntax.
+Creates a new instance and associates a geographic location in the
+form of a DNS LOC record with it.  Note that the location is not
+checked to be in valid DNS master file syntax.
 
 =back
 
@@ -61,11 +61,11 @@ sub new($$$) {
 
 =over 4
 
-=item location()
+=item C<location()>
 
-my $loc = $net->location();
+  my $loc = $net->location();
 
-Returns the location string that was passed to the new() method.
+Returns the location string that was passed to the C<new()> method.
 
 =cut
 
@@ -74,31 +74,32 @@ sub location($) {
   return($self->{location});
 }
 
-=item add_prefix($prefix)
+=item C<add_prefix($prefix)>
 
-eval { $net->add_prefix($prefix) } or die $@;
+  eval { $net->add_prefix($prefix) } or die $@;
 
-Adds the given IPAM::Prefix object to the registry of prefixes
+Adds the given L<IPAM::Prefix> object to the registry of prefixes
 associated with the network.  An exception is raised if this fails
 (see L<IPAM::Prefix>).
 
 =cut
+
 sub add_prefix($$) {
   my ($self, $prefix) = @_;
   $self->{prefix_r}->add($prefix);
 }
 
-=item  prefixes($sorter, $af)
+=item C<prefixes($sorter, $af)>
 
-my @prefixes = $net->prefixes();
-my @prefixes = $net->prefixes(undef, '6');
-my @prefixes = $net->prefixes(sub { my ($a, $b) = @_;
+  my @prefixes = $net->prefixes();
+  my @prefixes = $net->prefixes(undef, '6');
+  my @prefixes = $net->prefixes(sub { my ($a, $b) = @_;
                                     $a->ip() cmp $b->ip() });
 
 Returns a list of objects in the network's L<IPAM::Prefix::Registry>
-of the given address family or for all address families if $af is not
-defined.  A sorting method can be supplied as for the
-L<IPAM::Registry> methods things() and iterator().
+of the given address family or for all address families if C<$af> is
+not defined.  A sorting method can be supplied as for the
+L<IPAM::Registry> methods C<things()> and C<iterator()>.
 
 =cut
 
@@ -107,9 +108,9 @@ sub prefixes($$$) {
   return($self->{prefix_r}->things($sorter, $af));
 }
 
-=item  prefix_registry()
+=item C<prefix_registry()>
 
-my $registry = $net->prefix_registry();
+  my $registry = $net->prefix_registry();
 
 Returns the network's L<IPAM::Prefix::Registry>.
 
@@ -120,12 +121,12 @@ sub prefix_registry($) {
   return($self->{prefix_r});
 }
 
-=item add_address($address)
+=item C<add_address($address)>
 
-eval { $net->add_address($address) } or die $@;
+  eval { $net->add_address($address) } or die $@;
 
-Adds the L<IPAM::Address> object $address to the network's prefix by
-which it is covered.  An exception is raised if the address is not
+Adds the L<IPAM::Address> object C<$address> to the network's prefix
+by which it is covered.  An exception is raised if the address is not
 covered by any prefix associated with the network.  Note that this
 address will be part of the address map.
 
@@ -142,12 +143,13 @@ sub add_address($$) {
 			      $self->prefixes(undef, $address->af())).")\n";
 }
 
-=item find_address($ip)
+=item C<find_address($ip)>
 
-my $address = $net->find_address($ip);
+  my $address = $net->find_address($ip);
 
-Searches the L<IPAM::Address::Registry> of the network that matches
-the given L<NetAddr::IP> object $ip.
+Searches the L<IPAM::Prefix::Registry> for an L<IPAM::Address> object
+that matches the given L<NetAddr::IP> object C<$ip>.  Returns the
+object or undef if no match is found.
 
 =cut
 
@@ -156,11 +158,13 @@ sub find_address($$) {
   return($self->{prefix_r}->lookup_by_ip($ip));
 }
 
-=item  add_host($host)
+=item C<add_host($host)>
 
-eval { $net->add_host($host) } or die $@;
+  eval { $net->add_host($host) } or die $@;
 
-Adds the given L<IPAM::Host> object to the network's host registry.  An execption is raised if a host with the same name has already been registered.
+Adds the given L<IPAM::Host> object to the network's host registry.
+An execption is raised if a host with the same name has already been
+registered.
 
 =cut
 
@@ -169,12 +173,13 @@ sub add_host($$) {
   $self->{host_r}->add($host);
 }
 
-=item find_host($fqdn)
+=item C<find_host($fqdn)>
 
-my $host = $net->find_host($fqdn);
+  my $host = $net->find_host($fqdn);
 
 Searches the network's L<IPAM::Host::Registry> for an object that
-matches the given $fqdn.  Returns that object or false if not found.
+matches the given C<$fqdn>.  Returns that object or false if not
+found.
 
 =cut
 
@@ -183,9 +188,9 @@ sub find_host($$) {
   return($self->{host_r}->lookup($fqdn));
 }
 
-=item hosts()
+=item C<hosts()>
 
-my @hosts = $net->hosts();
+  my @hosts = $net->hosts();
 
 Returns a list of objects in the network's L<IPAM::Host::Registry>.
 
@@ -196,27 +201,27 @@ sub hosts($) {
   return($self->{host_r}->things());
 }
 
-=item host_r()
+=item C<host_registry()>
 
-my $registry = $net->host_r();
+  my $registry = $net->host_registry();
 
 Returns the network's L<IPAM::Host::Registry>.
 
 =cut
 
-sub host_r($) {
+sub host_registry($) {
   my ($self) = @_;
   return($self->{host_r});
 }
 
-=item find_alias($fqdn)
+=item C<find_alias($fqdn)>
 
-my $alias = $net->find_alias($fqdn);
+  my $alias = $net->find_alias($fqdn);
 
 Searches the registries of aliases of all hosts in the network for the
-FQDN $fqdn and returns the L<IPAM::Host> object of the canonical name
-or undef if no matching alias is found.  Note that an alias can have
-at most one canonical name.
+FQDN C<$fqdn> and returns the L<IPAM::Host> object of the canonical
+name or undef if no matching alias is found.  Note that an alias can
+have at most one canonical name.
 
 =cut
 
@@ -229,6 +234,14 @@ sub find_alias($$) {
   return(undef);
 }
 
+=back
+
+=head1 SEE ALSO
+
+L<IPAM::Thing>, L<IPAM::Prefix>, L<IPAM::Address>, L<NetAddr::IP>
+
+=cut
+
 package IPAM::Network::Registry;
 use IPAM::Registry;
 our @ISA = qw(IPAM::Registry);
@@ -240,7 +253,7 @@ IPAM::Network::Registry - Class of a registry for L<IPAM::Network> objects
 
 =head1 SYNOPSIS
 
-use IPAM::Network;
+  use IPAM::Network;
 
 =head1 DESCRIPTION
 
@@ -251,11 +264,11 @@ L<IPAM::Registry>.  It stores a list of L<IPAM::Network> objects.
 
 =over 4
 
-=item find_host($fqdn)
+=item C<find_host($fqdn)>
 
-my @hosts = $net_r->find_host($fqdn);
+  my @hosts = $net_r->find_host($fqdn);
 
-Calls the find_host() method of all L<IPAM::Network> objects in the
+Calls the C<find_host()> method of all L<IPAM::Network> objects in the
 registry and returns the list of matching L<IPAM::Host> objects.
 
 =cut
@@ -270,13 +283,13 @@ sub find_host($$) {
   return(@result);
 }
 
-=item find_alias($fqdn)
+=item C<find_alias($fqdn)>
 
-my @aliases = $net_r->find_alias($fqdn);
+  my @aliases = $net_r->find_alias($fqdn);
 
-Calls the find_alias() method of all L<IPAM::Network> objects in the
-registry and returns the L<IPAM::Host> object of the alias' canonical
-name or undef if such alias exists.
+Calls the C<find_alias()> method of all L<IPAM::Network> objects in
+the registry and returns the L<IPAM::Host> object of the alias'
+canonical name or undef if such alias exists.
 
 =cut
 
@@ -289,5 +302,13 @@ sub find_alias($$) {
   }
   return(undef);
 }
+
+=back
+
+=head1 SEE ALSO
+
+L<IPAM::Registry>, L<IPAM::Network>
+
+=cut
 
 1;
