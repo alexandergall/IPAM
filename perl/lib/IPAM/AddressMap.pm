8841d8481bb5c3660d57bd887a3c9e8e3ba4a41e
Alexander Gall <alexander.gall@switch.ch>
2012-09-04 07:02:46 +0000

    Updated perldoc.

--- perl/lib/IPAM/AddressMap.pm
+++ perl/lib/IPAM/AddressMap.pm
@@ -3,7 +3,7 @@
 #### Description:   IPAM::AddressMap class
 #### Author:        Alexander Gall <gall@switch.ch>
 #### Created:       Jun 5 2012
-#### RCS $Id:$
+#### RCS $Id: AddressMap.pm,v 1.1 2012/07/12 08:08:43 gall Exp gall $
 
 package IPAM::AddressMap;
 use IPAM::Thing;
@@ -16,14 +16,17 @@ IPAM::AddressMap - Class that describe an address map
 
 =head1 SYNOPSIS
 
-use IPAM::AddressMap;
+  use IPAM::AddressMap;
 
 =head1 DESCRIPTION
 
-The IPAM::AddressMap class is derived from IPAM::Thing.  It stores a
-hierarchy of L<IPAM::Prefix> objects.  Its purpose is to represent an
-organization's addressing plan from high-level functional blocks down
-to IP subnets (also called "stub nets").
+The L<IPAM::AddressMap> class is derived from L<IPAM::Thing>.  It
+holds a L<IPAM::Prefix::Registry>, storing a hierarchy of
+L<IPAM::Prefix> objects that represent an organization's addressing
+plan from high-level functional blocks down to IP subnets provided by
+the <address-map> element of an IPAM database.  It provides a facility
+to search the registry by prefix or C<id> (a non-unique identifier in
+the form of a DNS FQDN).
 
 =cut
 
@@ -38,17 +41,20 @@ sub new($$$$) {
 
 =over 4
 
-=item lookup_by_ip($ip)
+=item C<lookup_by_ip($ip)>
 
-my $prefix = $address_map->lookup_by_ip($ip);
-my ($prefix, @path) = $address_map->lookup_by_ip($ip);
+  my $prefix = $address_map->lookup_by_ip($ip);
+  my ($prefix, @path) = $address_map->lookup_by_ip($ip);
 
-If called in a scalar context, returns the uniqe IPAM::Prefix object
-that matches the NetAddr::IP object $ip or undef if no match is found.
-If called in a list context, it also returns the list of IPAM::Prefix
-objects that were traversed during the search.  The last element of
-this list is the most specific covering prefix of $ip.  The following
-code covers alle possible cases
+This method traverses the prefix registry recursively, looking for a
+L<IPAM::Prefix> object whose associated L<NetAddr::IP> object (as
+returned by its C<ip()> method) matches the supplied L<NetAddr::IP>
+object C<$ip>.  If called in a scalar context, it returns the
+L<IPAM::Prefix> or undef if no match is found.  If called in a list
+context, it also returns the list of L<IPAM::Prefix> objects that were
+traversed during the search.  The last element of this list is the
+most specific covering prefix of C<$ip>.  The following code covers
+alle possible cases
 
   my ($prefix, @path) = $address_map->lookup_by_ip($ip);
   unless ($prefix or @path) {
@@ -71,17 +77,18 @@ sub lookup_by_ip($$) {
   }
 }
 
-=item lookup_by_id($id)
+=item C<lookup_by_id($id)>
 
-my @prefixes = $address_map->lookup_by_id($id);
+  my @prefixes = $address_map->lookup_by_id($id);
 
-Returns a list of IPAM::Prefix objects whose id attribute matches the
-string $id.  The id attribute is derived from the "name" attribute of
-a <block> or <net> XML element in the IPAM database.  Note that the
-name of an IPAM::Prefix as returned by the name() instance method
-inherited from the IPAM::Thing class is, by convention, the
-human-readable CIDR representation of the prefix.  While the former is
-unique in the address map, the latter is not.
+Returns a list of L<IPAM::Prefix> objects whose C<id> attribute
+matches the string C<$id>.  The C<id> attribute is derived from the
+C<name> attribute of a C<< <block> >> or C<< <net> >> XML element in
+the IPAM database.  Note that the name of an L<IPAM::Prefix> as
+returned by the C<name()> instance method inherited from the
+L<IPAM::Thing> class is, by convention, the human-readable CIDR
+representation of the prefix.  While the former is unique in the
+address map, the latter is not.
 
 =cut
 
@@ -90,14 +97,12 @@ sub lookup_by_id($$$) {
   return($self->{prefix_r}->lookup_by_id($name, $stub_only));
 }
 
-=item registry()
+=item C<registry()>
 
-my $registry = $address_map->registry()
+  my $registry = $address_map->registry()
 
-Returns the IPAM::Prefix::Registry object associated with the address
-map.
-
-=back
+Returns the L<IPAM::Prefix::Registry> object associated with the
+address map.
 
 =cut
 
@@ -105,5 +110,13 @@ sub registry($) {
   my ($self) = @_;
   return($self->{prefix_r})
 }
+
+=back
+
+=head1 SEE ALSO
+
+L<IPAM::Thing>, L<IPAM::Prefix>, L<IPAM::Prefix::Registry>
+
+=cut
 
 1;
